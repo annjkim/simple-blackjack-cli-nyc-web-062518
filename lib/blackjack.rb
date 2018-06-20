9d8705cce0308d70d7570e7a506f55aee125893d
Ann Kim <annkim822@gmail.com>
2018-06-20 12:42:52 +0000

    automatically backed up by learn

--- lib/blackjack.rb
+++ lib/blackjack.rb
@@ -23,15 +23,7 @@ def end_game(card_total)
 end
 
 def initial_round
-  card_one = deal_card
-  card_two = deal_card
-  card_total = card_one + card_two
-  return card_total
-  display_card_total(card_total)
-  display_card_total = deal_card
-  display_card_total += deal_card
-  return display_card_total
-  end
+
 end
 
 def hit?(card_total)
